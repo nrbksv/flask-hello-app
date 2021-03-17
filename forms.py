2215f96cae12cf783d7587397ad52add3745c148
Dmitry Stepanov <stpnv.dmtry@gmail.com>
2021-03-17 20:34:45 +0600

    #2 Создана форма для статей

---
+++ forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField
+from wtforms.validators import length, required
+
+
+class ArticleForm(FlaskForm):
+    title = StringField(label='Заголовок', validators=(required(), length(min=5, max=150)))
+    content = TextAreaField(label='Контент', validators=(required(), length(min=20, max=1500)))
+    author = StringField(label='Автор', validators=(required(), length(min=3, max=120)))
