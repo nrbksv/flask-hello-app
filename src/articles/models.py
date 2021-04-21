9103eaced165c76350cc1912568238dd1d9e52cf
nrbk <nrbksv@gmail.com>
2021-04-21 16:44:08 +0600

    #14 Добавлена модель комментариев.

--- src/articles/models.py
+++ src/articles/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from src import db
 
 
@@ -6,6 +8,18 @@ class Article(db.Model):
     title = db.Column(db.String(150))
     content = db.Column(db.Text)
     author = db.Column(db.String(150))
+    comments = db.relationship('Comment', backref='comments')
 
     def __repr__(self):
         return f'<Article {self.id}: {self.title}>'
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.Text(2000))
+    author = db.Column(db.String(150))
+    article = db.Column(db.ForeignKey('article.id'))
+    created_at = db.Column(db.DateTime(), default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'{self.author}{self.comment}'
